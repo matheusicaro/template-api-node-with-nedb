31460fe6aff5e7dca39c72ab84201e83942e330d
Matheus Icaro <matheusicaro2@hotmail.com>
2018-09-10 21:46:46 -0300

    initialized server utilizing "express".

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res)=>{
+let app = express();
 
-    console.log("Url..:", req.url);
-    console.log("Method..:", req.method);
+app.get('/', (req, res) => {
 
-    switch(req.url){
-        
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end("<h1> ok <h1>")
-            break;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<h1> ok <h1>")
 
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                user:[{
-                    name: 'matheus',
-                    id: 1
-                }]
-            }));
-            break;
-    }
+})
 
-});
+app.get('/users', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        user:[{
+            name: 'matheus',
+            id: 1
+        }]
+    });
+})
 
 const port = 3000;
 const ip = '127.0.0.1';
 
-server.listen(port, ip, ()=>{
+app.listen(port, ip, ()=>{
 
     console.log("Listening in port ", port);
 });
